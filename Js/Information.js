ed7f11d373005f8c8967b547af61bd440404c6e9
IjodbekMehmonaliyev03 <IjodbekMehmonaliyev@gmail.com>
2022-06-06 15:05:06 +0500

    JavaScript-Info

---
+++ Js/Information.js
@@ -0,0 +1,45 @@
+// 1... /* Bir vaqtnig o'zida o'zgaruvchi ham satr ham raqam bo'lshi mumkin */
+
+// let message = "Hello"
+// message = 12345;
+// console.log(message);
+
+// 2... /* BigInt type ixtiyoriy uzunlikdagi butun sonlarni ifodalash uchun tilga yaqinda qo‘shildi. */
+
+// let bigInt = 1234567890123456789012345678901234567890n;
+// console.log(bigInt);
+
+// 3 ... // let str = "Hello";
+// let str2 = 'Single quotes are ok too';
+// let phrase = `can embed another ${str}`;
+// console.log(phrase);
+
+
+// 4... /* Backticks - bu "kengaytirilgan funksionallik" tirnoqlari. ${ ... }, */
+
+// let name = "John";
+
+// embed a variable
+// alert( `Hello, ${name}!` ); // Hello, John!
+
+// embed an expression
+// alert( `the result is ${1 + 2}` ); // the result is 3
+
+// 5... Boolean type - tur
+
+// let nameFieldChecked = true; // yes, name field is checked
+// let ageFieldChecked = false; // no, age field is not checked
+
+// console.log(ageFieldChecked);
+
+// 6....  Mantiqiy qiymatlar ham taqqoslash natijasida kelib chiqadi:
+
+// let isGreater = 4 > 1;
+
+// alert( isGreater ); true
+
+// 7... undefined
+
+// let age;
+
+// alert(age); // shows "undefined"
---
+++ Js/Interaction.js
@@ -0,0 +1,19 @@
+// ALERT PROMPT CONFIRM
+
+// 1... Funktsiya promptikkita argumentni qabul qiladi:
+
+// let age = prompt('How old are you?', );
+
+// alert(`You are ${age} years old!`);  
+
+// 2.. .Natija, OK bosilsa true va boshqacha bo'lsa agar  false . 
+
+// let isBoss = confirm("Are you the boss?");
+
+// alert( isBoss ); // true if OK is pressed   
+
+// 3... Ism so'raydigan va uni chiqaradigan web sahifa 
+
+// let name1 = prompt('Ismingizni Yozing', );
+// alert(name1);
+
